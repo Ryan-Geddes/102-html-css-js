732ca5387da328e24cde66270c1a331c085ad3af
Ryan Geddes <rgeddes09@gmail.com>
2020-03-04 19:01:46 -0800

    trying out functions

--- js/app.js
+++ js/app.js
@@ -4,25 +4,43 @@ var outsideStatus;
 function didYouLeave(){
     outsideStatus = prompt('HAVE YOU LEFT THE HOUSE THIS WEEK?');
     var message;
-    if (outsideStatus === 'yes'){
+    while ((outsideStatus !== 'yes')&&(outsideStatus!== 'no')) {
+        outsideStatus = prompt('yes or no');
+    }if (outsideStatus === 'yes'){
         message = '<p> WE ARE ALL GOING TO DIE! RUN FOR YOUR LIVES! PANIC PANIC PANIC ALERT </p>'
-    // } else if (outsideStatus !== 'yes'){
-    //     message = 'ok you\'re good.'
     } else {
         message = '<img class="container_panic" src="images/fine.jpg" alt="thisisfine">'
 }
 return document.write("<p>"+ message + "</p>");
+
 }
 
 function deathCalc(){
     if (outsideStatus === 'yes'){
         var timesOutside = prompt('Oh wow, no kidding?  How many times?');
-        var aBillion = 1000000000
-        var deathChance = timesOutside*aBillion;
-    return document.write("<p>" + 'damn son you have a ' + deathChance + '% chance of dying homie.' + "</p>");
-    } else {
-        message = 'Console report: you\'re ok!'
-    }
-
+    return timesOutside;
 }
 
+function getItem(){
+    var item = '<img class="container_panic" src="images/fine.jpg" alt="thisisfine">';
+    return item;
+}
+
+function chancesBillion() {
+    var timesOutside = deathCalc();
+    var aBillion = 1000000000
+    var deathChance = timesOutside*aBillion;
+    return document.write("<p>" + 'damn son you have a ' + deathChance + '% chance of dying homie.' + "</p>");
+}
+
+function showFine() {
+    var item = getItem();
+    var timesOutside = deathCalc();
+    result = '';
+
+for (var i = 0; i < timesOutside; i++){
+    result = result + '<p>' + item + '</p>';
+    }
+return document.write(result);
+}
+}
